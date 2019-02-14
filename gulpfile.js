addd2cfb918ffe7220b5e39d4cad28f9f2b395cd
zhangyv <zidea@163.com>
2019-02-14 15:20:26 +0800

    提交基础，删除 test

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+ console.log("go")
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
